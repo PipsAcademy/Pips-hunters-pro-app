55eee3c8c818f13374e82568abf3a502683b78a1
Pipshunter.support <pipshuntersupport@gmail.com>
2026-04-06 18:20:08 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const cacheName = 'pips-hunter-v1';
-const assets = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'pips-hunter-v3';
+const urlsToCache = [ '/', '/index.html', '/manifest.json', '/logo.png' ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+  event.waitUntil( caches.open(CACHE_NAME).then(cache => { return cache.addAll(urlsToCache); }) );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith( caches.match(event.request).then(response => { return response || fetch(event.request); }) );
 });
